86fb5eb656932d28b8aa593dff73b194872c3372
Anand Krishnan <iamanandkris@gmail.com>
2014-10-11 16:52:21 +0530

    Changing the file locations and adding more endpoints

--- blueprints/frontend/__init__.py
+++ blueprints/frontend/__init__.py
@@ -8,3 +8,16 @@ frontend = Blueprint('frontend', __name__,
 @frontend.route('/', methods=['GET'])
 def test_page():
     return render_template('test.html')
+    
+@frontend.route('/login', methods=['GET'])
+def test_page():
+    return render_template('Login.html')
+    
+@frontend.route('/createDonor', methods=['GET'])
+def test_page():
+    return render_template('DonorCreation.html')
+    
+@frontend.route('/donate', methods=['GET'])
+def test_page():
+    return render_template('DonationForm.html')
+
