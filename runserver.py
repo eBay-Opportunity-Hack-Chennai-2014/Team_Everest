a3c3cc460af183e124a7ba525a8af036d1dfa7af
traibhandare <tushar.raibhandare@gmail.com>
2014-10-11 17:52:53 +0530

    Basic database

--- runserver.py
+++ runserver.py
@@ -1,11 +1,18 @@
 from flask import Flask
 
 from blueprints.frontend import frontend
+from blueprints.backend import backend
+
+from models import db
 
 app = Flask(__name__)
 app.config.from_object('default_settings')
 app.config.from_envvar('EVEREST_SETTINGS', silent=True)
 app.register_blueprint(frontend, url_prefix='/')
 
+db.init_app(app)
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=8000)
